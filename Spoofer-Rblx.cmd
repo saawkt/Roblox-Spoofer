4940fb50257b7edb57a66b4f3da981bbfe25cc47
wasynxyxxk <202036399+saawkt@users.noreply.github.com>
2025-06-26 16:13:47 -0300

    2.0

--- Spoofer-Rblx.cmd
+++ Spoofer-Rblx.cmd
@@ -1,6 +1,6 @@
 @echo off
 title Roblox Unban, made by misenplase and saawkt
-taskkill /f /im RobloxPlayerBeta.exe /t 
+taskkill /f /im RobloxPlayerBeta.exe /t
 certutil -URLCache * delete
 netsh int ip reset
 netsh int ipv4 reset
@@ -13,6 +13,7 @@ netsh winsock reset
 netsh int ip reset
 netsh winsock reset
 netsh advfirewall reset
+
 ::tmac
 curl -L "https://github.com/saawkt/files/raw/refs/heads/main/tmac/oui.db" -o "C:\Windows\temp\oui.db"
 curl -L "https://github.com/saawkt/files/raw/refs/heads/main/tmac/TMAC.exe" -o "C:\Windows\temp\tmac.exe"
@@ -26,11 +27,19 @@ taskkill /f /im TMAC.exe /t
 del C:\Windows\temp\tmac.exe
 del C:\Windows\temp\oui.db 
 cls
+
+:: eventlog cleaner
 for /f "tokens=*" %%a in ('wevtutil el') do (
 wevtutil cl "%%a"
 )
 cls
+
+:: clear roblox cookies and data
 del /s /q /f %LocalAppData%\Roblox\LocalStorage\RobloxCookies.dat
+del /s /q /f %LocalAppData%\Roblox\LocalStorage\memProfStorage8688.json
+del /s /q /f %LocalAppData%\Roblox\LocalStorage\appStorage.json
+
+:: cloudflare warp
 set "warpPath=%ProgramFiles%\Cloudflare\Cloudflare WARP\Cloudflare WARP.exe"
 if exist "%warpPath%" (
 echo Cloudflare WARP founded, opening the app...
@@ -42,7 +51,6 @@ start /b /wait "" "C:\Windows\temp\warp.msi" /passive >nul 2>&1
 del C:\Windows\temp\warp.msi
 )
 echo your cloudflare warp has opened, is it in background, open to use
-echo if warp dont installed a link gonna be opened soon
-echo if done, just open and start service and close this cmd
+
 pause >nul
 exit /b 
